63320e0a25ec237e0063162d40268407d5dd08a2
Teddy Odhiambo <tedb19@gmail.com>
2019-08-24 19:47:45 +0300

    Added query for votes

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,9 @@
 const info = () => `War ready!`
 const feed = (parent, args, ctx, info) => ctx.prisma.links()
+const votes = (parent, args, ctx, info) => ctx.prisma.votes()
 
 module.exports = {
   feed,
-  info
+  info,
+  votes
 }
