ed76ecea1a50bbe31002b493406a89871d80cbbf
Teddy Odhiambo <tedb19@gmail.com>
2019-08-24 23:26:19 +0300

    Added filter for the feed query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,18 @@
 const info = () => `War ready!`
-const feed = (parent, args, ctx, info) => ctx.prisma.links()
+
+const feed = (parent, args, ctx, info) => {
+  const where = args.filter
+    ? {
+        OR: [
+          { description_contains: args.filter },
+          { url_contains: args.filter }
+        ]
+      }
+    : {}
+
+  return ctx.prisma.links({ where })
+}
+
 const votes = (parent, args, ctx, info) => ctx.prisma.votes()
 
 module.exports = {
